06339f9fc8386b6df6835b19bd750e410625ae5b
Null-ch <zuxobot@yandex.ru>
2025-12-31 10:44:08 +0300

    fix: sanctum

--- src/config/sanctum.php
+++ src/config/sanctum.php
@@ -1,7 +1,5 @@
 <?php
 
-use Laravel\Sanctum\Sanctum;
-
 return [
 
     /*
@@ -17,9 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'
     ))),
 
     /*
