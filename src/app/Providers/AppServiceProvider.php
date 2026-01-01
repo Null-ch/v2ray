673eea72822cec1307bf44c5b94d7b0e3805953d
Null-ch <zuxobot@yandex.ru>
2026-01-01 23:28:46 +0300

    feat: курсор написал хуйню

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use SergiX44\Nutgram\Nutgram;
+use App\Services\TelegramService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(TelegramService::class, function ($app) {
+            return new TelegramService();
+        });
+
+        $this->app->singleton(Nutgram::class, function ($app) {
+            return $app->make(TelegramService::class)->getBot();
+        });
     }
 
     /**
@@ -19,6 +27,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Пример регистрации обработчиков Telegram бота:
+        // $bot = $this->app->make(Nutgram::class);
+        // $bot->onCommand('start', function (Nutgram $bot) {
+        //     $bot->sendMessage('Привет!');
+        // });
         //
+        // Или используйте TelegramBotHandlers:
+        // $handlers = new \App\Services\TelegramBotHandlers($bot);
+        // $handlers->registerHandlers();
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TelegramWebhookController;
 use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('test', [TestController::class, 'create']);
 Route::put('test/{id}', [TestController::class, 'update']);
+
+Route::post('telegram/webhook', [TelegramWebhookController::class, 'handle']);
