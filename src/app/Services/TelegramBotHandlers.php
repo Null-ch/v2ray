c8e64fc48fd80d091d5c92a86cd8c409084a7b98
Бетабакс <>
2026-01-02 18:02:16 +0200

    переделал ссылки для первой инструкции

--- src/app/Services/TelegramBotHandlers.php
+++ src/app/Services/TelegramBotHandlers.php
@@ -70,24 +70,7 @@ final readonly class TelegramBotHandlers
                 // Генерируем конфиг (заглушка)
                 $this->userService->generateVpnConfig();
 
-                $instructionsKeyboard = InlineKeyboardMarkup::make()
-                    ->addRow(
-                        InlineKeyboardButton::make('Приложение для Android', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-                    )
-                    ->addRow(
-                        InlineKeyboardButton::make('Приложение для iOS', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-                    )
-                    ->addRow(
-                        InlineKeyboardButton::make('Для Windows', url: 'https://telegra.ph/Instrukciya-VPN-Windows-01-01')
-                    )
-                    ->addRow(
-                        InlineKeyboardButton::make('Перенести ключ в приложение', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-                    )
-                    ->addRow(
-                        InlineKeyboardButton::make('Вернуться в главное меню', callback_data: 'main_menu')
-                    );
-
-                $messageIds = $this->vpnConnectionService->sendVpnConnectionMessages($bot, $instructionsKeyboard);
+                $messageIds = $this->vpnConnectionService->sendVpnConnectionMessages($bot, $this->getInstructionsKeyboard());
 
                 $bot->setGlobalData('vpn_message_ids', $messageIds);
 
@@ -96,6 +79,32 @@ final readonly class TelegramBotHandlers
                 $bot->answerCallbackQuery('Произошла ошибка: ' . $e->getMessage(), show_alert: true);
                 throw $e;
             }
+        });
+
+        // Обработчик нажатия на кнопку "ПОДКЛЮЧИТЬ ВПН" для существующих пользователей
+        $this->bot->onCallbackQueryData('connect_vpn', function (Nutgram $bot) {
+            $messageIds = $this->vpnConnectionService->sendVpnConnectionMessages($bot, $this->getInstructionsKeyboard());
+
+            // Сохраняем ID сообщений в глобальные данные пользователя
+            $bot->setGlobalData('vpn_message_ids', $messageIds);
+
+            // Отвечаем на callback, чтобы убрать "часики" на кнопке
+            $bot->answerCallbackQuery();
+        });
+
+        // Обработчик кнопки "Перенести в приложение"
+        $this->bot->onCallbackQueryData('export_config', function (Nutgram $bot) {
+            // TODO: Генерация экспортной ссылки с конфигами
+            // Пример: https://www.sigmalink.org/redirect/?redirect_to=www.sigmalink.org&token=TOKEN&scheme=v2raytun
+
+            $exportUrl = 'https://www.sigmalink.org/redirect/?redirect_to=www.sigmalink.org&token=PLACEHOLDER_TOKEN&scheme=v2raytun';
+
+            $bot->sendMessage(
+                "📲 Экспортная ссылка для переноса конфигов:\n\n$exportUrl\n\n⚠️ Внимание: это заглушка, функционал в разработке",
+                parse_mode: 'HTML'
+            );
+
+            $bot->answerCallbackQuery('Ссылка отправлена');
         });
 
         // Обработчик кнопки "Вернуться в главное меню"
@@ -133,5 +142,24 @@ final readonly class TelegramBotHandlers
             $bot->sendMessage('Доступные команды:' . PHP_EOL . '/start - Начать работу' . PHP_EOL . '/help - Помощь');
         });
     }
-}
 
+    private function getInstructionsKeyboard(): InlineKeyboardMarkup
+    {
+        return InlineKeyboardMarkup::make()
+            ->addRow(
+                InlineKeyboardButton::make('Приложение для Android', url: 'https://play.google.com/store/apps/details?id=com.v2raytun.android&pcampaignid=web_share')
+            )
+            ->addRow(
+                InlineKeyboardButton::make('Приложение для iPhone/iOS', url: 'https://apps.apple.com/ru/app/v2raytun/id6476628951')
+            )
+            ->addRow(
+                InlineKeyboardButton::make('Инструкция для Windows', url: 'https://telegra.ph/Instrukciya-po-ustanovke-V2raytun-na-PK--Windows-1011-01-02')
+            )
+            ->addRow(
+                InlineKeyboardButton::make('📲 Перенести в приложение', callback_data: 'export_config')
+            )
+            ->addRow(
+                InlineKeyboardButton::make('Вернуться в главное меню', callback_data: 'main_menu')
+            );
+    }
+}
