d2b549f3bf6b7af57d90d3348b10f15155956929
Null-ch <zuxobot@yandex.ru>
2026-01-03 12:10:42 +0300

    fix: app.php

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->global([
+            \App\Http\Middleware\TrustProxies::class,  // <-- добавляем сюда
+        ]);
         $middleware->api(prepend: [
             ForceJsonResponse::class,
         ]);
