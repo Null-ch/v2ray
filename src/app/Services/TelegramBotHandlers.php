f7d2bae940d2e2df7d8c68716cf4b0de510486e2
Null-ch <zuxobot@yandex.ru>
2026-01-04 14:37:19 +0300

    test

--- src/app/Services/TelegramBotHandlers.php
+++ src/app/Services/TelegramBotHandlers.php
@@ -70,7 +70,7 @@ final readonly class TelegramBotHandlers
                 $bot->sendMessage('⏳ Создаю VPN конфигурацию, пожалуйста, подождите...');
 
                 // Ставим задачу в очередь для асинхронной обработки
-                ProcessAcceptTermsJob::dispatch($telegramId, $username, $name);
+                ProcessAcceptTermsJob::dispatch($bot, $telegramId, $username, $name);
             } catch (\Throwable $e) {
                 Log::error('Ошибка при постановке задачи accept_terms в очередь: ' . $e->getMessage(), [
                     'trace' => $e->getTraceAsString(),
