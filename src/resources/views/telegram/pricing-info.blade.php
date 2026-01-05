d517a5ec6745ae6b57690cd4b657caf681bd27dc
Null-ch <zuxobot@yandex.ru>
2026-01-05 14:44:41 +0300

    fix template

--- src/resources/views/telegram/pricing-info.blade.php
+++ src/resources/views/telegram/pricing-info.blade.php
@@ -1,10 +1,8 @@
 {{ $name }}, выберите тарифный план для VPN {{ $tag }}
 
 @foreach($pricings as $pricing)
-
 ⏱ Длительность: {{ $pricing->title }}
 💰 Цена: {{ number_format($pricing->price, 2, '.', ' ') }} ₽
-
 @endforeach
 
 Выберите тарифный план для оплаты:
