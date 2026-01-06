e7c063eae7fa8b9e4dbea14d0807b67f11a57848
Null-ch <zuxobot@yandex.ru>
2026-01-07 00:39:19 +0300

    fix

--- src/app/Http/Controllers/TelegramWebhookController.php
+++ src/app/Http/Controllers/TelegramWebhookController.php
@@ -21,7 +21,6 @@ final readonly class TelegramWebhookController
     {
         try {
             $data = $request->all();
-            Log::info('Telegram webhook data: '. json_encode($data));
             $update = Update::fromArray($data);
             $this->telegramService->getBot()->processUpdate($update);
         } catch (\Throwable $e) {
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\TelegramWebhookController;
-use Illuminate\Http\Request;
 
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
@@ -13,15 +11,10 @@ use Illuminate\Http\Request;
 Route::post('test', [TestController::class, 'create']);
 Route::put('test/{id}', [TestController::class, 'update']);
 
-// if (!empty(config('services.telegram.bot_token'))) {
-//     Log::info('Webhook hit', $request->all());
-//     Route::post('telegram/webhook', [TelegramWebhookController::class, 'handle']);
-// }
+if (!empty(config('services.telegram.bot_token'))) {
+    Route::post('telegram/webhook', [TelegramWebhookController::class, 'handle']);
+}
 
-Route::post('telegram/webhook', function(Request $request) {
-    Log::info('Webhook hit', $request->all());
-    return response()->json(['ok' => true]);
-});
 // Payment API routes
 Route::prefix('payment')->group(function () {
     Route::post('/', [\App\Http\Controllers\PaymentController::class, 'create'])->name('api.payment.create');
