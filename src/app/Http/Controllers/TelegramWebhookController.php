e468a9ac9b47122e4d5b16f7aaff77105d4ddb12
Null-ch <zuxobot@yandex.ru>
2026-01-07 00:50:59 +0300

    1

--- src/app/Http/Controllers/TelegramWebhookController.php
+++ src/app/Http/Controllers/TelegramWebhookController.php
@@ -21,7 +21,7 @@ final readonly class TelegramWebhookController
     {
         try {
             $data = $request->all();
-            Log::indo('Пришел реквест: ' . json_encode($data));
+            Log::info('Пришел реквест: ' . json_encode($data));
             $update = Update::fromArray($data);
             $this->telegramService->getBot()->processUpdate($update);
         } catch (\Throwable $e) {
