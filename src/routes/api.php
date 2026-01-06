04efbb080378cbb0f6353a38dce34a5a6942eb10
Null-ch <zuxobot@yandex.ru>
2026-01-07 00:43:54 +0300

    1

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\TelegramWebhookController;
+use SergiX44\Nutgram\Nutgram;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
 
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
@@ -11,9 +15,23 @@ use App\Http\Controllers\TelegramWebhookController;
 Route::post('test', [TestController::class, 'create']);
 Route::put('test/{id}', [TestController::class, 'update']);
 
-if (!empty(config('services.telegram.bot_token'))) {
-    Route::post('telegram/webhook', [TelegramWebhookController::class, 'handle']);
-}
+// if (!empty(config('services.telegram.bot_token'))) {
+//     Route::post('telegram/webhook', [TelegramWebhookController::class, 'handle']);
+// }
+
+Route::post('/api/telegram/webhook', function(Request $request) {
+    Log::info('Webhook hit', $request->all());
+
+    $bot = new Nutgram(config('services.telegram.bot_token'));
+
+    $bot->onCommand('start', function(Nutgram $bot) {
+        $bot->sendMessage("Привет! Бот работает!");
+    });
+
+    $bot->run(); // обязательно запускаем обработку обновлений
+
+    return response()->json(['ok' => true]);
+});
 
 // Payment API routes
 Route::prefix('payment')->group(function () {
