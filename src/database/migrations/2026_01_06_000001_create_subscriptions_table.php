5662207a849e13ed3234cdf78badc7b193bcd6f9
Null-ch <zuxobot@yandex.ru>
2026-01-06 23:47:53 +0300

    фикс миграции

--- src/database/migrations/2026_01_06_000001_create_subscriptions_table.php
+++ src/database/migrations/2026_01_06_000001_create_subscriptions_table.php
@@ -26,7 +26,6 @@ return new class extends Migration
                 ->on('xuis')
                 ->onDelete('cascade');
 
-            $table->index(['user_id', 'tag']);
             $table->unique(['user_id', 'xui_id', 'uuid']);
         });
     }
