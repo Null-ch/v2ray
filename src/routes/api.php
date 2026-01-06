67981c00692cd5241d85f595b03cf05aea3c77a5
Null-ch <zuxobot@yandex.ru>
2026-01-07 00:30:34 +0300

    1

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\TelegramWebhookController;
+use Illuminate\Http\Request;
 
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
@@ -12,8 +13,15 @@ use App\Http\Controllers\TelegramWebhookController;
 Route::post('test', [TestController::class, 'create']);
 Route::put('test/{id}', [TestController::class, 'update']);
 
-Route::post('telegram/webhook', [TelegramWebhookController::class, 'handle']);
+// if (!empty(config('services.telegram.bot_token'))) {
+//     Log::info('Webhook hit', $request->all());
+//     Route::post('telegram/webhook', [TelegramWebhookController::class, 'handle']);
+// }
 
+Route::post('/api/telegram/webhook', function(Request $request) {
+    Log::info('Webhook hit', $request->all());
+    return response()->json(['ok' => true]);
+});
 // Payment API routes
 Route::prefix('payment')->group(function () {
     Route::post('/', [\App\Http\Controllers\PaymentController::class, 'create'])->name('api.payment.create');
