3ac547bde2d352fe510bfe2010f2b15d398cfddd
Null-ch <zuxobot@yandex.ru>
2026-01-08 22:53:37 +0300

    fix

--- src/resources/views/cockpit/partials/users_table.blade.php
+++ src/resources/views/cockpit/partials/users_table.blade.php
@@ -10,26 +10,8 @@
     <td>
       <div class="btn-list">
         <a class="btn btn-primary btn-sm btn-glass" href="{{ route('cockpit.key.index', ['user_id' => $user->id]) }}" title="Выдать ключ">
-          <i class="ti ti-key-plus"></i>
+          <i class="ti ti-key"></i>
         </a>
-        <button type="button"
-                class="btn btn-success btn-sm btn-glass btn-user-balance-sign"
-                data-uid="{{ $user->id }}"
-                data-username="{{ $user->name ?? $user->tg_tag ?? 'N/A' }}"
-                data-balance="{{ number_format($user->balance->balance ?? 0, 2) }}"
-                data-sign="+"
-                title="Начислить баланс">
-          <i class="ti ti-plus"></i>
-        </button>
-        <button type="button"
-                class="btn btn-danger btn-sm btn-glass btn-user-balance-sign"
-                data-uid="{{ $user->id }}"
-                data-username="{{ $user->name ?? $user->tg_tag ?? 'N/A' }}"
-                data-balance="{{ number_format($user->balance->balance ?? 0, 2) }}"
-                data-sign="-"
-                title="Списать баланс">
-          <i class="ti ti-minus"></i>
-        </button>
         <a href="{{ route('cockpit.user.show', $user->id) }}" class="btn btn-info btn-sm btn-glass" title="Просмотр">
           <i class="ti ti-eye"></i>
         </a>
